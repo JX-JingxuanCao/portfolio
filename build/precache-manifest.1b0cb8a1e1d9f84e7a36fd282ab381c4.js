15f4bf09da7e1ad9702a21798c502a8c47c8d50d
JX <42965701+JX-JingxuanCao@users.noreply.github.com>
2020-03-11 19:33:34 +1100

    first! yeah -JX & Mitch

---
+++ build/precache-manifest.1b0cb8a1e1d9f84e7a36fd282ab381c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b764282af6d95719eeba205114601f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "284c05fdc2527b287234",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2c7d8c481f42f9704bd",
+    "url": "/static/js/2.23b2c6ae.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.23b2c6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284c05fdc2527b287234",
+    "url": "/static/js/main.1b51690b.chunk.js"
+  },
+  {
+    "revision": "8506d302c9af5ccf4c27",
+    "url": "/static/js/runtime-main.b9a77842.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
