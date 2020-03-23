d33f4a575f1f4461d5effcb619a9ffdfafd611ac
Jingxuan Cao <cjingxuan@outlook.com>
2020-03-23 22:39:17 +1100

    Toogle button updated, almost there - JX

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -48,38 +48,38 @@ export default function Contact() {
         </div>
       </div>
 
-      <div classNames="credit-line">
+      <div className="credit-line">
         <svg
-          width="820"
+          width="1020"
           height="1"
-          viewBox="0 0 820 1"
+          viewBox="0 0 1020 1"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
           <line
-            x1="820"
+            x1="1020"
             y1="0.5"
-            x2="-1.22031e-05"
-            y2="0.499928"
+            x2="-2.82126e-07"
+            y2="0.499911"
             stroke="#989898"
           />
         </svg>
       </div>
-      <div className="credit-box">
-        <p className="credit-text">Copyright 2020 © Jingxuan Cao</p>
-        <p className="credit-text">
-          React consultant: Mitch Hartigan, check out his awesome portfolio
-        </p>
-      </div>
-      <div class="social-buttons">
-        <a
-          class="social-button website"
-          href="https://mitchhartigan.com/"
-          target="_blank"
-          title="Mitch's Portfolio"
-        >
-          <i class="fa fa-globe"></i>
-        </a>
+      <div className="lastBox">
+        <div className="credit-box">
+          <p className="credit-text">Copyright 2020 © Jingxuan Cao</p>
+          <p className="credit-text">
+            React consultant: Mitch Hartigan, check out his awesome portfolio
+            {/* <a
+              class="social-button website"
+              href="https://mitchhartigan.com/"
+              target="_blank"
+              title="Mitch's Portfolio"
+            >
+              <i class="fa fa-globe"></i>
+            </a> */}
+          </p>
+        </div>
       </div>
     </div>
   );
---
+++ src/Components/toggleButton.js
@@ -0,0 +1,116 @@
+import React from "react";
+import SkillSet from "./skillSet.js";
+
+export default class ToogleButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayPG: false
+    };
+    this.state = {
+      displaySoso: false
+    };
+    this.state = {
+      displayLearn: false
+    };
+    this.state = {
+      displayThissite: false
+    };
+  }
+
+  displayPG = () => {
+    this.setState({
+      displayPG: true,
+      displaySoso: false,
+      displayLearn: false,
+      displayThissite: false
+    });
+  };
+
+  displaySoso = () => {
+    this.setState({
+      displayPG: false,
+      displaySoso: true,
+      displayLearn: false,
+      displayThissite: false
+    });
+  };
+
+  displayLearn = () => {
+    this.setState({
+      displayPG: false,
+      displaySoso: false,
+      displayLearn: true,
+      displayThissite: false
+    });
+  };
+
+  displayThissite = () => {
+    this.setState({
+      displayPG: false,
+      displaySoso: false,
+      displayLearn: false,
+      displayThissite: true
+    });
+  };
+
+  render() {
+    if (this.state.displayPG) {
+      console.log("button clicked!");
+      return (
+        <div>
+          <button onClick={this.displayPG}>HO yeah baby</button>
+          <button onClick={this.displaySoso}>Click Me PLZ!</button>
+          <button onClick={this.displayLearn}>DO it!</button>
+          <button onClick={this.displayThissite}>JUST DO IT!</button>
+        </div>
+      );
+    }
+    if (this.state.displaySoso) {
+      console.log("button clicked!");
+      return (
+        <div>
+          <button onClick={this.displayPG}>Click Me!</button>
+          <button onClick={this.displaySoso}>This is so cool</button>
+          <button onClick={this.displayLearn}>DO it!</button>
+          <button onClick={this.displayThissite}>JUST DO IT!</button>
+        </div>
+      );
+    }
+    if (this.state.displayLearn) {
+      console.log("button clicked!");
+      return (
+        <div>
+          <button onClick={this.displayPG}>Click Me!</button>
+          <button onClick={this.displaySoso}>Click Me PLZ!</button>
+          <button onClick={this.displayLearn}>God damn it works</button>
+          <button onClick={this.displayThissite}>JUST DO IT!</button>
+        </div>
+      );
+    }
+    if (this.state.displayThissite) {
+      console.log("button clicked!");
+      return (
+        <div>
+          {" "}
+          <button onClick={this.displayPG}>Click Me!</button>
+          <button onClick={this.displaySoso}>Click Me PLZ!</button>
+          <button onClick={this.displayLearn}>DO it!</button>
+          <button onClick={this.displayThissite}>
+            LOL!!MITCH U ARE A GENIUS
+          </button>
+        </div>
+      );
+    }
+    console.log("button not clicked!");
+    return (
+      <div>
+        <button onClick={this.displayPG}>Click Me!</button>
+        <button onClick={this.displaySoso}>Click Me PLZ!</button>
+        <button onClick={this.displayLearn}>DO it!</button>
+        <button onClick={this.displayThissite}>JUST DO IT!</button>
+      </div>
+    );
+  }
+}
+// ReactDOM.render(<TodoApp />, document.querySelector("#app"))
