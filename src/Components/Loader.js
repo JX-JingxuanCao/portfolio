caf101f03b27440e5d635617559376a1e53f1783
Jingxuan Cao <cjingxuan@outlook.com>
2020-06-01 12:00:53 +1000

    Testing new loader #2 - JX

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -10,7 +10,7 @@ class Loader extends React.Component {
             displayMessage: false,
         };
 
-        this.timer = setTimeout(this.enableMessage, 250);
+        this.timer = setTimeout(this.enableMessage, 6000);
     }
 
     componentWillUnmount() {
